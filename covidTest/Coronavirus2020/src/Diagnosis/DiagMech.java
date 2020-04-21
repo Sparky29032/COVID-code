540324ee09f69f2c8dba7cd72db5f3c68301f40a
liuemi001 <64052980+liuemi001@users.noreply.github.com>
2020-04-20 21:36:51 -0500

    Add files via upload

---
+++ covidTest/Coronavirus2020/src/Diagnosis/DiagMech.java
@@ -0,0 +1,14 @@
+package Diagnosis;
+
+public class DiagMech {
+	
+	double riskfactor;
+	double risklevel = 0;
+	//risk level 0 means not at risk, 1 is light risk, 2 is likely case, 3 is certain case
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
